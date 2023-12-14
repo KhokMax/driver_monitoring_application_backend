09b9ccf8bf2c050bda473c520f218e15b6c251ba
KhokMax <khok.max.2002@gmail.com>
2023-12-14 23:47:08 +0200

    --

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ api.add_resource(Drivers, "/driver", "/driver/<user_id>")
 api.init_app(app)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=3000, host='127.0.0.1')
+    app.run(debug=True, port=5000, host='0.0.0.0')
