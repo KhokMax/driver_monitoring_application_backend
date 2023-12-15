54743b1b1b8015b25e32a2a53daef7dc033b260d
KhokMax <khok.max.2002@gmail.com>
2023-12-15 15:59:29 +0200

    s

--- app.py
+++ app.py
@@ -15,10 +15,11 @@ api = Api()
 
 class Main(Resource):
     def get(self):
-        engine = create_engine("postgresql+psycopg2://postgres:IvNa2023OlKh@34.168.253.165:5432/postgres")
-        connection = engine.connect();
-        df = pd.read_sql(get_all_deliveries, connection);
-        return df.to_json(orient="index")
+        # engine = create_engine("postgresql+psycopg2://postgres:IvNa2023OlKh@34.168.253.165:5432/postgres")
+        # connection = engine.connect();
+        # df = pd.read_sql(get_all_deliveries, connection);
+        # return df.to_json(orient="index")
+        return "ewfewfwe"
 
 api.add_resource(Main, "/main")
 api.add_resource(Drivers, "/driver", "/driver/<user_id>")
