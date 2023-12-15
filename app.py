2d7b657a1c5b654a621ac54f405a00a08acb510c
KhokMax <khok.max.2002@gmail.com>
2023-12-15 17:23:32 +0200

    cors edited

--- app.py
+++ app.py
@@ -7,24 +7,18 @@ from  queries import *
 from  drivers import *
 
 app = Flask(__name__)
-api = Api()
-CORS(app)
-# conn = psycopg2.connect(host="34.168.253.165", port="5432", dbname="postgres", password="IvNa2023OlKh", user="postgres")
-# cursor = conn.cursor()
-
-
+api = Api(app)  # Передайте екземпляр вашого Flask застосунку в конструктор Api
+CORS(app)  # Використовуйте CORS для вашого застосунку
 
 class Main(Resource):
     def get(self):
         engine = create_engine("postgresql+psycopg2://avnadmin:AVNS_zb-76Zov-eh6OfnbW-Z@driver-monitoring-application-db-khok-8eb3.a.aivencloud.com:19713/defaultdb")
-        connection = engine.connect();
-        df = pd.read_sql(get_all_deliveries, connection);
+        connection = engine.connect()
+        df = pd.read_sql(get_all_deliveries, connection)
         return df.to_json(orient="index")
-        #return "ewfewfwe"
 
 api.add_resource(Main, "/main")
 api.add_resource(Drivers, "/driver", "/driver/<user_id>")
-api.init_app(app)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host='0.0.0.0')
