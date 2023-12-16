3141d83ce2c0a08c8dcb737c13019b74125e2be8
KhokMax <khok.max.2002@gmail.com>
2023-12-16 15:26:43 +0200

    --

--- localrequest.py
+++ localrequest.py
@@ -14,10 +14,10 @@ import requests
 # res = requests.post("http://localhost:5000/driver", json=data)
 # print(res)
 
-# res = requests.delete("https://driver-monitoring-application-d48766f2cbe0.herokuapp.com/driver/ed575401-b48e-4cdd-a998-71e7cd7347a7")
+# res = requests.delete("https://driver-monitoring-application-d48766f2cbe0.herokuapp.com/driver/1")
 # print(res.json())
 
-res = requests.delete("https://localhost:500/driver/ed575401-b48e-4cdd-a998-71e7cd7347a7")
+res = requests.delete("https://driver-monitoring-application-d48766f2cbe0.herokuapp.com/driver/1")
 print(res.json())
 
 # data = {'driver_first_name': "dwq", 'driver_last_name':"qwd", 'driver_patronymic': "bty4", 'driver_age': 43, 'driver_rank': "reb", 'mobile_phone': "435423", 'mail': "dqwdfre", 'a1_category': True, 'b1_category': True, 'login': "user1", 'password': "x4mws89Jf"}
