7248d9c1d700fcf6a61a3dd2df49060e1cb8fda9
KhokMax <khok.max.2002@gmail.com>
2023-12-18 20:50:54 +0200

    --

--- deliveries.py
+++ deliveries.py
@@ -18,21 +18,29 @@ class Deliveries(Resource):
             values = [delivery_id if item == "delivery_id" else request.json.get(item, None) for item in column_list]
             sql_query = f"INSERT INTO deliveries ({', '.join(column_list)}) VALUES ({', '.join(':' + item for item in column_list)})"
 
-            with create_engine("postgresql+psycopg2://avnadmin:AVNS_zb-76Zov-eh6OfnbW-Z@driver-monitoring-application-db-khok-8eb3.a.aivencloud.com:19713/defaultdb").connect() as connection:
-                try:
-                    connection.execute(text(sql_query), dict(zip(column_list, values)))
-                    connection.commit()
-                except Exception as e:
-                    connection.rollback()
-                    print(e)
-                    return {"Exception": "404", "Description": "Database interaction error"}
+            engine = create_engine("postgresql+psycopg2://avnadmin:AVNS_zb-76Zov-eh6OfnbW-Z@driver-monitoring-application-db-khok-8eb3.a.aivencloud.com:19713/defaultdb", poolclass=pool.QueuePool)
+            connection = engine.connect()
+                
+            connection.execute(text(sql_query), dict(zip(column_list, values)))
+            connection.commit()
+
+            connection.close()
+            engine.dispose()
         
         except Exception as e:
+            try:
+                connection.rollback()
+                connection.close()
+                engine.dispose()
+            except Exception as e:
                 print(e)
-                return {"Exeption": "404"}
-
+                return {"Exception": "404"}
+            
+            print(e)
+            return {"Exception": "404", "Description": "Database interaction error"}
+        
         return {"Response": "200"}
-    
+
     
     def get(self):
         try:
